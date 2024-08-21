2b3e466ded4006d70451baab49f8da0f44454315
Cody Vong <me@example.com>
2024-08-20 17:18:40 -0700

    Add project files.

---
+++ FahrenheitToCelsiusInteractive/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+class FahrenheitToCelsiusInteractive
+{
+    static void Main()
+    {
+        string Fahrenheit;
+        double inputNumber;
+        double Celsius;
+        Write("Enter Fahrenheit degrees >> ");
+        Fahrenheit = ReadLine();
+        inputNumber = Convert.ToDouble(Fahrenheit);
+        Celsius = (inputNumber - 32) * 5 / 9;
+        string Celsius1 = Celsius.ToString("F1");
+        WriteLine(Fahrenheit + " degrees Fahrenheit is " + Celsius1 + " degrees Celsius");
+    }
+}
